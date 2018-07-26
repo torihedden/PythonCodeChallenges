15c04725f09e036fc4f00371c777e790a07c8ac3
Tori Hedden <tori.hedden@gmail.com>
2018-07-26 14:33:11 -0400

    Update warmup2answers.py

--- warmup2answers.py
+++ warmup2answers.py
@@ -14,3 +14,12 @@
 #   for x in range(n):
 #     times_str += str[0:3]
 #   return times_str
+
+
+# Warmup-2 > string_bits 
+
+# def string_bits(str):
+#   new_str = ''
+#   for x in range(0, len(str), 2):
+#     new_str += str[x]
+#   return new_str
