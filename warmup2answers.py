d001444ca7fe5cec7f31544ce9de8c03122390f9
Tori Hedden <tori.hedden@gmail.com>
2018-07-26 13:59:16 -0400

    Update warmup2answers.py

--- warmup2answers.py
+++ warmup2answers.py
@@ -1,7 +1,16 @@
 # Warmup-2 > string_times 
 
-def string_times(str, n):
-  loop_str = ''
-  for x in range(n):
-    loop_str += str
-  return loop_str
+# def string_times(str, n):
+#   loop_str = ''
+#   for x in range(n):
+#     loop_str += str
+#   return loop_str
+
+
+# Warmup-2 > front_times 
+
+# def front_times(str, n):
+#   times_str = ''
+#   for x in range(n):
+#     times_str += str[0:3]
+#   return times_str
